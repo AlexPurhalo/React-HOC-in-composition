50a214ce90b043c6ad617dcc7187738aaf33fa0a
AlexPurhalo <alexpurhalo@gmail.com>
2019-06-14 17:24:51 +0300

    markup-version-1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    songs: []
+  }
+
+  componentDidMount() {
+    fetch('/songs.json')
+      .then(response => response.json())
+      .then(json => json)
+      .then(songs => this.setState({songs}))
+  }
+
+  render() {
+    const { songs } = this.state
+    return (
+      <div id="container">
+        <header id="pageHeader">
+          <input/>
+        </header>
+        <nav id="nav">
+          <ul>
+            {songs.map(({ title, artist }) => (
+              <li>{artist} - {title}</li>
+            ))}
+          </ul>
+        </nav>
+        <div id="content">
+          {songs.map(({ picture, title, artist}, i) => (
+            <div key={i}>
+              <div>
+                <img className="picture" src={picture} alt=""/>
+                <div>{artist}???????????</div>
+                <div>{title}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <footer>Footer</footer>
+      </div>
+    )
+  }
 }
+export default App
 
-export default App;
