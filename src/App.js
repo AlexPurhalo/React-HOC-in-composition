d56bd00e2c7de1259d0659deec88e0652c0052ba
AlexPurhalo <alexpurhalo@gmail.com>
2019-06-18 09:19:12 +0300

    App structure refactoring

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import App             from './components/App'
-import withSongs       from './containers/withSongs'
-import withActions     from './containers/withActions'
-import withAudioPlayer from './containers/withAudioPlayer'
-import withRefs				 from './containers/withRefs'
-import withLogs				 from './containers/withLogs'
-import { compose } 		 from './utils'
+import React, { forwardRef } from 'react'
+import styled from 'styled-components'
+import { withSongs, withActions, withAudioPlayer, withRefs } from './containers'
+import { SearchBar, SongsList, Player } from './components'
+import { compose } from './utils'
 
-export default compose(withSongs, withActions, withRefs, withAudioPlayer)(App)
+const Wrapper = styled.section`
+  color: #333;
+  background: #e25f5fa1;
+  display: grid;
+  grid-template-areas:
+          "header"
+          "content"
+          "footer";
+  grid-template-rows: 60px 10fr 60px;
+  grid-template-columns: 1fr;
+  grid-gap: 2px;
+  height: 100vh;
+  margin: 0;
+`
+
+const App = (props, ref) => (
+	<Wrapper>
+		<SearchBar {...props}	/>
+		<SongsList {...{...props, ref}} />
+		<Player    {...{...props, ref}} />
+	</Wrapper>
+)
+
+export default compose(withSongs, withActions, withRefs, withAudioPlayer, forwardRef)(App)
---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+import withSongs       from './withSongs'
+import withActions     from './withActions'
+import withAudioPlayer from './withAudioPlayer'
+import withRefs				 from './withRefs'
+
+export { withSongs, withActions, withAudioPlayer, withRefs }
