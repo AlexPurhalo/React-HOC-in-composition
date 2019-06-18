429eec876a8751354c6d0631bf0588c9057f5f95
AlexPurhalo <alexpurhalo@gmail.com>
2019-06-18 12:39:29 +0300

    Search Bar Markup

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,28 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Header = styled.header`
-	background: white;
+	background: #fff6f3;
 	grid-area: header;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	text-align: center;
 `
 
-const SearchBar = () => <Header />
+const Input = styled.input`
+	height: 95%;
+	width: 70%;
+	font-size: 25px;
+	border: solid 2px #e25f5fa1;
+	border-bottom: solid 2px inherit;
+	text-align: center;
+	&:focus { outline: 0 };
+`
+
+const SearchBar = () => (
+	<Header>
+		<Input type="text" placeholder="start typing for tracks" />
+	</Header>
+)
 
 export default SearchBar
