c87d6d1bed1a151f7d33de1761598d18803741c7
AlexPurhalo <alexpurhalo@gmail.com>
2019-06-18 17:13:11 +0300

    Utils refactoring

--- src/utils.js
+++ src/utils.js
@@ -1,28 +1,44 @@
-export const compose = (...rest) => x => rest.reduceRight((y, f) => f(y), x)
+export const compose = (...rest) => x => {
+	return rest.reduceRight((y, f) => f(y), x)
+}
 
-export const adjustTimeStr = (str) => str
-	.replace(/(\d+):(\d+)/g, (_, p1, p2) =>
-		`${p1.length < 2 ? 0+p1 : p1}:${p2.length < 2 ? 0+p2 : p2}`)
+export const adjustTimeStr = (str) => {
+	return str.replace(/(\d+):(\d+)/g, (_, p1, p2) => {
+		return `${p1.length < 2 ? 0+p1 : p1}:${p2.length < 2 ? 0+p2 : p2}`
+	})
+}
 
-export const preparePlayingTime = (time) =>
-	`${Math.floor(time/60)}:${Math.floor(time%60)}`
-
-
-export const findSong = (songs, songId) => songs.find(({ id }) => id === songId)
-
-export const findNextSong = (songs, song) => songs[songs.indexOf(song)+1]
-
-export const findPrevSong = (songs, song) => songs[songs.indexOf(song)-1]
+export const preparePlayingTime = (time) => {
+	return `${Math.floor(time/60)}:${Math.floor(time%60)}`
+}
 
 
-export const prepSearchRegexp = (search) =>
-	new RegExp(search.replace(/\\/g, ''), 'i')
 
-export const searchForTracks = (songs, regexp) =>
-	songs.filter(({ artist, title }) => regexp.test(artist) || regexp.test(title))
+export const findSong = (songs, songId) => {
+	return songs.find(({ id }) => id === songId)
+}
+
+export const findNextSong = (songs, song) => {
+	return songs[songs.indexOf(song)+1] || songs[0]
+}
+
+
+export const findPrevSong = (songs, song) => {
+	return songs[songs.indexOf(song)-1] || songs[songs.length-1]
+}
+
+export const prepSearchRegexp = (search) => {
+	return new RegExp(search.replace(/\\/g, ''), 'i')
+}
+
+export const searchForTracks = (songs, regexp) => {
+	return songs.filter(({ artist, title }) => {
+		return regexp.test(artist) || regexp.test(title)
+	})
+}
 
 export const computeCurrVal = (currX, fullX, fullY) => {
+	return fullX && currX / fullX * fullY
 	// size/fullSize = currAm/fullAm
 	// size = currAm / fullAm * fullSize
-	return fullX && currX / fullX * fullY
 }
